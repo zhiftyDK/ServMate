0aff2a1c184cdf9ba909d9e8975b50f76e611115
CodeZhifty <juulstausholmoscar@gmail.com>
2025-05-06 05:07:36 +0200

    Changed folder creation directory

--- manager/index.js
+++ manager/index.js
@@ -10,21 +10,6 @@ const http = require('http'); // Import the http module
 
 const HTTP_PORT = 4500;
 
-try {
-    const fs = require('fs');
-    const path = require('path');
-
-    const dataDir = path.join(__dirname, 'data');
-    if (!fs.existsSync(dataDir)) {
-        fs.mkdirSync(dataDir, { recursive: true });
-        console.log('Folder created:', dataDir);
-    } else {
-        console.log('Folder already exists:', dataDir);
-    }
-} catch (err) {
-    console.error('Error creating folder:', err);
-}
-
 manager.startManager({ verbose: true });
 
 const app = express();
--- manager/modules/db.js
+++ manager/modules/db.js
@@ -2,6 +2,11 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
+const dataDir = path.join(__dirname, '../data');
+if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir, { recursive: true });
+}
+
 const dbPath = path.join(__dirname, '../data', 'user.db');
 const db = new Database(dbPath);
 
